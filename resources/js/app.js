2210dd21f26e302456b1e606f7546638159e5270
jeremihz <mihjere68@gmail.com>
2019-08-10 19:50:21 +0300

    updated dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,24 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+
+
+
+let routes = [
+    { path: '/Dashboard', component: require('./components/Dashboard.vue').default },
+     { path: '/Profile', component: require('./components/Profile.vue').default },
+  
+]
+
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -29,4 +47,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -10,12 +10,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
+     <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>AdminLTE 3 | Starter</title>
 
 <link rel="stylesheet"  href="css/app.css">
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
 
@@ -70,14 +72,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
 
-        <li class="nav-item">
-         <a href="#" class="nav-link">
-        <i class="nav-icon fas fa-tachometer-alt blue"></i>
+     <li class="nav-item">
+        <router-link to="/Dashboard" class="nav-link">
+          <i class="nav-icon fas fa-user yellow"></i>
          <p>
-        Dashboard
+           Dashboard
         </p>
-        </a>
+              </router-link>
           </li>
+          
 
           
           <li class="nav-item has-treeview menu-open">
@@ -104,14 +107,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </ul>
           </li>
            <li class="nav-item">
-         <a href="#" class="nav-link">
+        <router-link to="/Profile" class="nav-link">
         <i class="nav-icon fas fa-user yellow"></i>
          <p>
       profile
         </p>
-        </a>
+              </router-link>
           </li>
 
+
+                    
            <li class="nav-item">
          <a href="#" class="nav-link">
         <i class="nav-icon fa fa-power-off red"></i>
@@ -138,6 +143,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
+
+        <router-view></router-view>
         
               </div>
             </div><!-- /.card -->
