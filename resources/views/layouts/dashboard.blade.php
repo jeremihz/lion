1b521a7162d4253f67d973da6dee5ed1681ab11c
jeremihz <mihjere68@gmail.com>
2019-08-11 00:38:19 +0300

    dashboard finishing

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -74,7 +74,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
      <li class="nav-item">
         <router-link to="/Dashboard" class="nav-link">
-          <i class="nav-icon fas fa-user yellow"></i>
+          <i class="nav-icon fas fa-tachometer-alt blue"></i>
          <p>
            Dashboard
         </p>
@@ -108,7 +108,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
            <li class="nav-item">
         <router-link to="/Profile" class="nav-link">
-        <i class="nav-icon fas fa-user yellow"></i>
+        <i class="nav-icon fas fa-user orange"></i>
          <p>
       profile
         </p>
@@ -116,14 +116,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
 
-                    
-           <li class="nav-item">
-         <a href="#" class="nav-link">
-        <i class="nav-icon fa fa-power-off red"></i>
-         <p>
-     Logout
-        </p>
-        </a>
+
+        <a class="nav-link" href="{{ route('logout') }}"
+                 onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+                           <i class="nav-icon fa fa-power-off red"></i>
+                           <p>
+                             {{ __('Logout') }}</p>
+                              </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
           </li>
         
         </ul>
